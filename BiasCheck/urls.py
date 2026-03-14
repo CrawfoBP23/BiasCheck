a9f9889fe522060cf7b49da7556d5f5eaef6c9ee
CrawfoBP23 <CrawfoBP23@gmail.com>
2026-03-14 09:57:10 -0400

    Update URL patterns for homepage and API

--- BiasCheck/urls.py
+++ BiasCheck/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('analyzer.urls')),
+
+    path('', include('analyzer.urls')),   # homepage + UI
+    path('api/', include('analyzer.urls')) # API endpoints
 ]
