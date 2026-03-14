1a3cb1d5c0fe732532b2c471262e7e0cddb392a8
CrawfoBP23 <CrawfoBP23@gmail.com>
2026-03-14 09:56:07 -0400

    Add search_news path to URLs

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import analyze_article, home
+from .views import home, search_news, analyze_article
 
 urlpatterns = [
+    path('', home),
+    path('search/', search_news),
     path('analyze/', analyze_article),
-    path("", home)
 ]
