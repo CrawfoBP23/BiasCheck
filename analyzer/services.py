91383b281e3d598dd037bfc3adc836d1717d0db3
CrawfoBP23 <CrawfoBP23@gmail.com>
2026-03-14 10:01:23 -0400

    Add function to fetch related news articles

---
+++ analyzer/services.py
@@ -0,0 +1,38 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+
+
+def get_related_news(topic):
+
+    url = "https://newsapi.org/v2/everything"
+
+    params = {
+        "q": topic,
+        "apiKey": NEWS_API_KEY,
+        "pageSize": 5,
+        "language": "en"
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        return []
+
+    data = response.json()
+
+    articles = []
+
+    for article in data.get("articles", []):
+
+        articles.append({
+            "title": article["title"],
+            "source": article["source"]["name"],
+            "url": article["url"]
+        })
+
+    return articles
